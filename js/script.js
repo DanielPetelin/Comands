cbc6120df4e45f15a2e1783fe1f8f8048f26e695
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-14 14:31:48 +0300

    add new

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let aboutMe = '';
+let out = '';
+
+$(document).ready(function() {
+    $(document).on('keydown', startCollect);
+});
+
+function startCollect(e) {
+    if (e.key == '`') {
+        console.log('start typing commands');
+        $(document).off('keydown', startCollect);
+        $(document).on('keydown', collectSimbol);
+    };
+};
+
+function collectSimbol(e) {
+    if (e.key == 'Escape') {
+        stopCollect();
+    } else {
+        aboutMe += e.key;
+        console.log(aboutMe);
+        if (aboutMe == 'activatemod') {
+            $('#exampleModal').modal({
+                show : true
+            });
+        };
+        // if (aboutMe == "alertShift('") {
+        //     out += e.key;
+        //     aboutMe + out;
+        //     if (aboutMe == "alertShift('" + out + "'Shift)") {
+        //         alert(out);
+        //     }
+        // };
+        if (aboutMe == "alertShift('hello'Shift)") {
+            alert('hello');
+        };
+    };
+};
+
+function stopCollect() {
+    console.log('stop');
+    aboutMe = '';
+    $(document).off('keydown', collectSimbol);
+    $(document).on('keydown', startCollect);
+};
